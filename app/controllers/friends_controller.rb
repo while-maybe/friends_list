5a933d13a5c3d2432de6ee86bb59669b9430c582
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-14 02:02:44 +0100

    feat: add Associations

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -65,6 +65,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.expect(friend: [ :first_name, :last_name, :email, :phone, :twitter ])
+      params.expect(friend: [ :first_name, :last_name, :email, :phone, :twitter, :user_id ])
     end
 end
