ab9774938b70e5e6fd3e8e747f1a99e6720cbea3
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-15 01:43:38 +0100

    feat: add More Associations

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,11 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
 
+  before_action :authenticate_user!, except: [ :index, :show ]
   # GET /friends or /friends.json
+
+  before_action :correct_user, only: [ :edit, :update, :destroy ]
+
   def index
     @friends = Friend.all
   end
@@ -12,7 +16,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +26,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -55,6 +61,11 @@ class FriendsController < ApplicationController
       format.html { redirect_to friends_path, status: :see_other, notice: "Friend was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not Allowed to Edit" if @friend.nil?
   end
 
   private
